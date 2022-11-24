1eef6b93e7853b2ab08906d43228618537731f1e
Jay <jaylin8@gmail.com>
2022-11-24 15:09:12 -0500

    docker start script

--- document/docker/docker-start.sh
+++ document/docker/docker-start.sh
@@ -3,3 +3,14 @@
 rm ~/mydata/tc/elasticsearch/plugins/.DS_Store
 
 docker compose -f docker-compose-env.yml up -d
+
+# init logstash
+cat <<EOF | docker exec logstash /bin/bash
+logstash-plugin install logstash-codec-json_lines
+EOF
+
+docker restart logstash
+
+
+# start apps
+# docker compose -f docker-compose-app.yml up -d
