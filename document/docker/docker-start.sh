2e005d832415a174530cf2b9ab0d05e93794c501
Jay Lin <jaylin8@gmail.com>
2022-12-03 23:09:42 -0500

    merged docker start

--- document/docker/docker-start.sh
+++ document/docker/docker-start.sh
@@ -4,7 +4,7 @@ rm ~/mydata/tc/elasticsearch/plugins/.DS_Store
 
 docker network create docker-main-net
 
-docker compose -f docker-compose-env.yml up -d
+docker compose -f docker-compose-env.yml up --build --force-recreate -d
 
 # init logstash
 cat <<EOF | docker exec logstash /bin/bash
